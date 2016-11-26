ee89442bd2ed408573bf7073cbe1f52fbd1b9e2d
Chad <chadtwelch@gmail.com>
2016-11-26 08:16:22 -0500

    Finished basic requirements.

--- app.js
+++ app.js
@@ -5,4 +5,19 @@
 
   LunchCheckerController.$inject = ['$scope'];
 
+  function LunchCheckerController ($scope){
+    $scope.inputString = "";
+
+    $scope.checkIfTooMuch = function () {
+      if($scope.inputString == ""){
+        $scope.message = "Please enter data first";
+      }
+      else if($scope.inputString.split(",").length <= 3){
+        $scope.message = "Enjoy!"
+      }
+      else {
+        $scope.message = "Too Much!"
+      }
+    };
+  };
 })();
