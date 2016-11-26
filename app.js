89e1bb1f4ca157663f4c7ff2485c304f124a52a4
Chad <chadtwelch@gmail.com>
2016-11-26 09:27:56 -0500

    Coded some optional requirements.

--- app.js
+++ app.js
@@ -7,16 +7,32 @@
 
   function LunchCheckerController ($scope){
     $scope.inputString = "";
+    $scope.splitString = $scope.inputString.split(",");
+    $scope.numOfFoods = 0;
 
     $scope.checkIfTooMuch = function () {
+      $scope.splitString = $scope.inputString.split(",");
+      $scope.numOfFoods = 0;
+
+      for(var i = 0; i < $scope.splitString.length; i ++){
+        if($scope.splitString[i] != ""){
+            $scope.numOfFoods++;
+        }
+      }
+
       if($scope.inputString == ""){
         $scope.message = "Please enter data first";
+        $scope.color = "red";
       }
-      else if($scope.inputString.split(",").length <= 3){
-        $scope.message = "Enjoy!"
-      }
-      else {
-        $scope.message = "Too Much!"
+      else{
+        $scope.color = "green";
+
+        if($scope.numOfFoods <= 3){
+          $scope.message = "Enjoy!"
+        }
+        else {
+          $scope.message = "Too Much!"
+        }
       }
     };
   };
